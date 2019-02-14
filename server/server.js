c1c3218e35f4a4c26df3b9cdafe95b79745d6fe5
ssura <svensura@germanscreens.de>
2019-02-14 12:58:17 +0100

    Greet new user and alert others

--- server/server.js
+++ server/server.js
@@ -14,11 +14,19 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected');
 
-  // socket.emit('newEmail', {
-  //   from: 'mike@example.com',
-  //   text: 'Some text',
-  //   createdAt: 123
-  // });
+  socket.emit('newMessage', {
+    from: 'Admin',
+    text: 'Welcome to the chat app',
+    createdAt: new Date().getTime()
+  });
+
+  socket.broadcast.emit('newMessage', {
+    from: 'Admin',
+    text: 'New user joined',
+    createdAt: new Date().getTime()
+  });
+
+
 
   // socket.emit('newMessage', {
   //   from: 'server',
@@ -37,6 +45,7 @@ io.on('connection', (socket) => {
       text: newMessage.text,
       createdAt: new Date().getTime()
     })
+
   });
 
   socket.on('disconnect', () => {
