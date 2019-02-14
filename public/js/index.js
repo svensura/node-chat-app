429a312ee9a312e68320ed1de07a0e79896ea6df
ssura <svensura@germanscreens.de>
2019-02-14 11:08:55 +0100

    Add newMessage and createMessage-Events

---
+++ public/js/index.js
@@ -0,0 +1,27 @@
+var socket = io();
+
+socket.on('connect', function () {
+    console.log('Connected to server');
+
+    // socket.emit('createEmail', {
+    //     to: 'jen@example.com',
+    //     text: 'Hey, this is Andrew.'
+    // })
+
+    socket.emit('createMessage', {
+        from: 'client',
+        text: 'Hey, this is the client.'
+    })
+})
+
+socket.on('disconnect', function () {
+    console.log('Disconnected from server');
+});
+
+// socket.on('newEmail', function (email) {
+//     console.log('New email ', email);
+// });
+
+socket.on('newMessage', function (message) {
+    console.log('newMessage ', message);
+});
--- server/server.js
+++ server/server.js
@@ -14,6 +14,27 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected');
 
+  // socket.emit('newEmail', {
+  //   from: 'mike@example.com',
+  //   text: 'Some text',
+  //   createdAt: 123
+  // });
+
+  socket.emit('newMessage', {
+    from: 'server',
+    text: 'From server text',
+    createdAt: 123
+  });
+
+  // socket.on('createEmail', (newEmail) => {
+  //   console.log('create Email', newEmail);
+  // });
+
+  socket.on('createMessage', (newMessage) => {
+    console.log('createMessage', newMessage);
+
+  });
+
   socket.on('disconnect', () => {
     console.log('User was disconnected');
   });
